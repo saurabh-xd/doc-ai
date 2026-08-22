b12f593cb5691dc101947941b4e2a8f4e5ecec4d
saurabh garkoti <saurabhgarkotiii@gmail.com>
2026-08-22 21:40:15 +0530

    Add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.documents import router as documents_router
 from app.api.chat import router as chat_router
@@ -11,6 +12,16 @@ app = FastAPI(
 )
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 #This attaches the routes from both modules into the main app.
 app.include_router(documents_router)
 app.include_router(chat_router)
